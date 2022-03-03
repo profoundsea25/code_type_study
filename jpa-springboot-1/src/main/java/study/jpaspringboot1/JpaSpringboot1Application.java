914f8221f2d70758e4672e84ef8441dd73689cd2
profoundsea25 <profoundsea25@gmail.com>
2022-03-03 19:04:11 +0900

    2022.03.03 JpaSpringboot1 시작

---
+++ jpa-springboot-1/src/main/java/study/jpaspringboot1/JpaSpringboot1Application.java
@@ -0,0 +1,13 @@
+package study.jpaspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSpringboot1Application.class, args);
+    }
+
+}
