9ce3f6b0d64942db1d97f395517a462ff3a6120e
profoundsea25 <profoundsea25@gmail.com>
2022-03-10 23:47:36 +0900

    2022.03.10 SpringMVC1, 1~41

---
+++ spring-mvc-1/1_servlet/src/main/java/study/springmvc1_1_servlet/SpringMvc11ServletApplication.java
@@ -0,0 +1,15 @@
+package study.springmvc1_1_servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan // 서블릿 자동 등록
+@SpringBootApplication
+public class SpringMvc11ServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc11ServletApplication.class, args);
+    }
+
+}
---
+++ spring-mvc-1/1_servlet/src/test/java/study/springmvc1_1_servlet/SpringMvc11ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springmvc1_1_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc11ServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
